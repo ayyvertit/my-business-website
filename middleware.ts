f6991c5282f19761863e7a83a0f8f6af61a167ae
ayyvertit <cammaveritt@gmail.com>
2025-09-22 12:25:13 -0400

    chore(auth): make Clerk middleware a no-op when CLERK_SECRET_KEY is missing

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
 import { clerkMiddleware } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
-export default clerkMiddleware();
+const hasClerkSecret = !!process.env.CLERK_SECRET_KEY;
+
+export default hasClerkSecret
+  ? clerkMiddleware()
+  : function middleware() {
+      return NextResponse.next();
+    };
 
 export const config = {
   matcher: [
